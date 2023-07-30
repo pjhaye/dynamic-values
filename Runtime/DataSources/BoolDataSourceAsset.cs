5e3d4bfdedfafd95021483cd2af80e042967a111
PJ Haye <pj.haye@gmail.com>
2023-07-30 11:31:07 -0700

    Ensure initial value for BoolDataSourceAsset is a SerializedField

--- Runtime/DataSources/BoolDataSourceAsset.cs
+++ Runtime/DataSources/BoolDataSourceAsset.cs
@@ -5,6 +5,7 @@ namespace DynamicValues.DataSources
     [CreateAssetMenu(menuName = "Dynamic Values/Data Sources/Bool Data Source Asset")]
     public class BoolDataSourceAsset : DataSourceAsset<bool>
     {
+        [SerializeField]
         private bool _initialValue;
 
         protected override bool InitialValue => _initialValue;
